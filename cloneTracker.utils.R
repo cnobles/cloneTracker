37957d56fc89ef0abb0df667e319fc11fad5cf01
nobles <chris.l.nobles@gmail.com>
2015-07-20 22:04:10 -0400

    Added a deamplify function which removes pcr replicates generated from the PCR amplification process to increase sensitivity. Optimized intSiteStandardize using graph theory.

--- cloneTracker.utils.R
+++ cloneTracker.utils.R
@@ -79,6 +79,19 @@ test <- GRanges(seqnames = Rle(values = c("chr1", "chr2", "chr3"),
   return(sites.clustered)
 }
 
+#samples need to have PCR replicates removed to reduce the amount of work needed to be done
+intSiteDeamplify <- function(sites){
+  sites.df <- as.data.frame(sites)
+  sites.df <- distinct(sites.df)
+  ranges <- IRanges(start = sites.df$start, end = sites.df$end)
+  sites.deamp <- GRanges(seqnames = sites.df$seqnames,
+                         ranges = ranges,
+                         strand = sites.df$strand,
+                         seqinfo = seqinfo(sites))
+  sites.deamp
+}
+
+
 #requires dplyr::distinct and sonicLength::estAbund
 intSiteAbund <- function(sites, method="fragLen"){
   sites$posID <- generate_posID(sites)
@@ -175,57 +188,76 @@ intSiteStandardize <- function(sites.unstandardized, window.size=5L,
          refering to the correct column in GRanges object.")
   }
   
-  sites.standardized <- lapply(sites.gp, function(sites){
-    sites$calledStart <- ifelse(strand(sites) == "+", start(sites), end(sites)) 
+  sites.standardized <- lapply(1:length(sites.gp), function(i){
+    sites <- sites.gp[[i]]
     sites$sonicBreak <- ifelse(strand(sites) == "+", end(sites), start(sites))
+    
+    #Manipulate ranges to only have unique starts (intSites), remove breakpoint and abundance info.
     sites.fl <- flank(sites, width = -1, start = TRUE)
-    overlaps <- findOverlaps(sites.fl, maxgap = window.size, select = "all", 
-                             ignoreSelf = TRUE, ignoreRedundant = TRUE)
+    sites.rd <- reduce(sites.fl, min.gapwidth = 0L, with.revmap = TRUE)
+    sites.rd$calledStart <- ifelse(strand(sites.rd) == "+", start(sites.rd), end(sites.rd))
+    sites.rd$freq <- sapply(sites.rd$revmap, length)
+    overlaps <- findOverlaps(sites.rd, maxgap = window.size, select = "all", 
+                             ignoreSelf = FALSE, ignoreRedundant = FALSE)
     edgelist <- matrix(c(queryHits(overlaps), subjectHits(overlaps)), ncol = 2)
     clusters <- clusters(graph.edgelist(edgelist, directed = FALSE))
-    sites.fl$clusterID <- clusters$membership
-    sites.fl <- split(sites.fl, sites.fl$clusterID)
-    sites.clus <- lapply(1:length(sites.fl), function(i){
-      clus <- sites.fl[[i]]
-      clus$readID <- seq(1:length(clus))
-      freq.df <- as.data.frame(table(clus$calledStart))
-      clus.df <- as.data.frame(mcols(clus))
-      clus.df <- merge(clus.df, freq.df, by.x="calledStart", by.y="Var1")
     
-    #At this point, position and frequency could be used to look at distribution
-    #Currently, the logic uses the same as hiReadsProcessor::clusterSites(), which
-    #favors the highest frequency (likely most common). I've added a mean calculation
-    #for sites that are clustered together spanning greater than the windowSize, this
-    #should be looked at more closely with test data sets. Lastly, in the case of equivalent
-    #frequency and within the windowSize, the upstream position is prefered, as with previous
-    #functions. This clusterStart calling logic should be looked at more closely, with test
-    #data sets.
-    
-      top.freq <- clus.df[clus.df$Freq == max(clus.df$Freq),]
-      if(length(unique(top.freq$posID)) == 1){
-        clus.position <- unique(top.freq$calledStart)
-      }else if((range(top.freq$calledStart)[2] - range(top.freq$calledStart)[1]) > window.size){
-        message("Possible bimodal distribution of intSites in cluster ", i, ".")
-        clus.position <- as.integer(mean(top.freq$calledStart))
-      }else{
-        clus.position <- min(top.freq$calledStart)
-      }
-      clus.df$clusterStart <- clus.position
-      clus.df <- arrange(clus.df, readID)
-      mcols(clus) <- clus.df
-      clus$readID <- NULL
-      clus
-    })
+    if(length(clusters$membership) > 0){
+      sites.rd$clusterID <- paste0(i, ":", clusters$membership)
+      sites.rd <- split(sites.rd, sites.rd$clusterID)
+      sites.clus <- lapply(1:length(sites.rd), function(j){
+        clus <- sites.rd[[j]]
+        
+        #At this point, position and frequency could be used to look at distribution
+        #Currently, the logic uses the same as hiReadsProcessor::clusterSites(), which
+        #favors the highest frequency (likely most common). I've added a mean calculation
+        #for sites that are clustered together spanning greater than the windowSize, this
+        #should be looked at more closely with test data sets. Lastly, in the case of equivalent
+        #frequency and within the windowSize, the upstream position is prefered, as with previous
+        #functions. This clusterStart calling logic should be looked at more closely, with test
+        #data sets.
+        
+        top.freq <- clus[clus$freq == max(clus$freq),]
+        if(length(top.freq) == 1){
+          clus.position <- unique(top.freq$calledStart)
+        }else if((range(top.freq$calledStart)[2] - range(top.freq$calledStart)[1]) > window.size){
+          message("Possible bimodal distribution of intSites in cluster ", j, ":", i, ".")
+          clus.position <- as.integer(mean(top.freq$calledStart))
+        }else{
+          clus.position <- min(top.freq$calledStart)
+        }
+        clus$clusterStart <- clus.position
+        clus
+      })
+    }else{
+      message("No sites within window.size, no clustering needed")
+      sites.rd$clusterID <- paste0(i, ":", seq(1:length(sites.rd)))
+      sites.rd$clusterStart <- sites.rd$calledStart
+      sites.clus <- list(sites.rd)
+    }
+
     sites.clus <- do.call(c, lapply(1:length(sites.clus), function(i){sites.clus[[i]]}))
-    ranges <- IRanges(start = ifelse(strand(sites.clus) == "+", 
-                                     sites.clus$clusterStart, sites.clus$sonicBreak),
-                      end = ifelse(strand(sites.clus) == "+",
-                                   sites.clus$sonicBreak, sites.clus$clusterStart))
-    sites.std <- GRanges(seqnames = seqnames(sites.clus),
+    
+    sites.fl <- sites.fl[unlist(sites.clus$revmap)]
+    sites.fl$clusterStart <- as.integer(Rle(values = sites.clus$clusterStart,
+                                            lengths = sites.clus$freq))
+    
+    ranges <- IRanges(start = ifelse(strand(sites.fl) == "+", 
+                                     sites.fl$clusterStart, sites.fl$sonicBreak),
+                      end = ifelse(strand(sites.fl) == "+",
+                                   sites.fl$sonicBreak, sites.fl$clusterStart))
+    sites.std <- GRanges(seqnames = seqnames(sites.fl),
                          ranges = ranges,
-                         strand = strand(sites.clus),
-                         seqinfo = seqinfo(sites.clus))
-    if(keep.mcols){mcols(sites.std) <- mcols(sites.clus)}
+                         strand = strand(sites.fl),
+                         seqinfo = seqinfo(sites.fl))
+    
+    if(keep.mcols){
+      mcols(sites.std) <- mcols(sites[unlist(sites.clus$revmap)])
+      sites.std$clusterStart <- as.integer(Rle(values = sites.clus$clusterStart,
+                                               lengths = sites.clus$freq))
+      sites.std$clusterID <- as.character(Rle(values = sites.clus$clusterID,
+                                              lengths = sites.clus$freq))
+      }
   
     sites.std
   })
