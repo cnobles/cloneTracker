b2893619207cb981420fad606ae470349fdaa470
cnobles <chris.l.nobles@gmail.com>
2015-07-28 14:17:57 -0400

    Added a few lines to make grouping easier in the functions.

--- functions/normalize_multihit_clusters.R
+++ functions/normalize_multihit_clusters.R
@@ -1,36 +1,54 @@
-normalize_multihit_clutsers <- function(multihit.gr){
+normalize_multihit_clutsers <- function(multihit.gr, grouping = NULL){
   #Multihits must be standardized and have clusterID info
-  key <- data.frame("multihitID" = multi.gr$multihitID,
-                    "clusterID" = as.character(multi.gr$clusterID)) 
+  if(is.null(grouping)){
+    multihits.gp <- list(multihit.gr)
+  }else if(grouping %in% names(mcols(multihit.gr))){
+    groups <- mcols(multihit.gr)[
+      grep(grouping, names(mcols(multihit.gr)))]
+    multihits.gr$groups <- groups[,1]
+    multihits.gp <- split(multihits.gr, multihits.gr$groups)
+  }else{
+    stop("Grouping partitioning failed. Make sure grouping is either NULL or 
+         refering to the correct column in GRanges object.")
+  }
   
-  clusterID_to_position <- data.frame("clusterID" = unique(key$clusterID), 
-                                      "position" = seq(1:length(unique(key$clusterID))))
+  std.multi.gr <- lapply(multihits.gp, function(gr){
+    key <- data.frame("multihitID" = gr$multihitID,
+                      "clusterID" = as.character(gr$clusterID)) 
   
-  std.clusID <- merge(key, clusterID_to_position, by.x="clusterID", by.y="clusterID")
+    clusterID_to_position <- data.frame("clusterID" = unique(key$clusterID), 
+                                        "position" = seq(1:length(unique(key$clusterID))))
   
-  multi.art.gr <- GRanges(seqnames = Rle("art_chr", nrow(key)),
-                          ranges = IRanges(start = key$position, width = 1),
-                          strand = "*",
-                          multihitID = key$multihitID,
-                          clusterID = key$clusterID)
+    std.clusID <- merge(key, clusterID_to_position, by.x="clusterID", by.y="clusterID")
   
-  multi.art.gl <- split(multi.art.gr, multi.art.gr$multihitID)
-  overlaps <- findOverlaps(multi.art.gl, minoverlap = 0L, select = "all", 
+    multi.art.gr <- GRanges(seqnames = Rle("art_chr", nrow(key)),
+                            ranges = IRanges(start = key$position, width = 1),
+                            strand = "*",
+                            multihitID = key$multihitID,
+                            clusterID = key$clusterID)
+  
+    multi.art.gl <- split(multi.art.gr, multi.art.gr$multihitID)
+    overlaps <- findOverlaps(multi.art.gl, minoverlap = 0L, select = "all", 
                            ignoreSelf = FALSE, ignoreRedundant = FALSE)
-  edgelist <- matrix(c(queryHits(overlaps), subjectHits(overlaps)), ncol = 2)
-  clusters <- clusters(graph.edgelist(edgelist, directed = FALSE))
+    edgelist <- matrix(c(queryHits(overlaps), subjectHits(overlaps)), ncol = 2)
+    clusters <- clusters(graph.edgelist(edgelist, directed = FALSE))
   
-  clus.dfr <- data.frame("multihitID" = names(multi.art.gl),
-                         "multihitID2" = clusters$membership)
+    clus.dfr <- data.frame("multihitID" = names(multi.art.gl),
+                         "stdmultihitID" = clusters$membership)
   
-  key <- merge(key, clus.dfr, by.x = "multihitID", by.y = "multihitID")
-  multi.list <- split(multi.gr, multi.gr$multihitID)
-  multi.list <- lapply(multi.list, function(x){
-    multihitID <- unique(x$multihitID)
-    multihitID2 <- key[key$multihitID == multihitID, 4]
-    x$multihitID2 <- multihitID2
-    x
+    key <- merge(key, clus.dfr, by.x = "multihitID", by.y = "multihitID")
+    multi.list <- split(gr, gr$multihitID)
+    multi.list <- lapply(multi.list, function(x){
+      multihitID <- unique(x$multihitID)
+      stdmultihitID <- key[key$multihitID == multihitID, 4]
+      x$stdmultihitID <- stdmultihitID
+      x
+    })
+    gr <- do.call(c, lapply(1:length(multi.list), function(i){multi.list[[i]]}))
+    gr
   })
-  multi.gr <- do.call(c, lapply(1:length(multi.list), function(i){multi.list[[i]]}))
-  multi.gr
+  
+  std.multi.gr <- do.call(c, lapply(1:length(std.multi.gr), function(i){std.multi.gr[[i]]}))
+  if(!is.na(grouping)){std.multi.gr$groups <- NULL}
+  std.multi.gr
 }
