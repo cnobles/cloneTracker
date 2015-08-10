68b2a569b02e270b3b7903ebb4e84c5052799087
cnobles <chris.l.nobles@gmail.com>
2015-08-10 16:37:37 -0400

    Split the sourcing script from the utils script.

---
+++ cloneTracker.SOURCE_ME.R
@@ -0,0 +1,34 @@
+#Dependancies check, if required packages are not loaded, script is haulted
+dependancies <- c("dplyr", "IRanges", "GenomicRanges", "igraph")
+
+sapply(dependancies, function(package){
+  library(package, character.only = TRUE)})
+
+dependancies_present <- sapply(dependancies, function(package){
+  package <- paste0("package:", package)
+  logic <- package %in% search()
+})
+
+if(FALSE %in% dependancies_present){
+  Unloaded_Packages <- data.frame(package=as.character(dependancies), 
+                                  loaded=dependancies_present)
+  stop("Load required packages. Check Unloaded_Packages for missing
+       dependancies.")
+}else{
+  remove(dependancies, dependancies_present)
+  message("Required packages loaded.")
+}
+
+
+#Source function scripts
+function_scripts <- c("track_clones.R", 
+                      "determine_abundance.R", 
+                      "standardize_intsites.R", 
+                      "normalize_multihit_clusters.R",
+                      "remove_repeats.R",
+                      "db_to_granges.R",
+                      "condense_intsites.R",
+                      "cloneTracker.utils.R",
+                      "ex_Data/test_GRanges.R")
+sapply(function_scripts, function(path){source(file = path)})
+message("cloneTracker functions loaded.")
--- cloneTracker.utils.R
+++ cloneTracker.utils.R
@@ -1,36 +1,3 @@
-#Dependancies check, if required packages are not loaded, script is haulted
-dependancies <- c("dplyr", "IRanges", "GenomicRanges", "igraph")
-
-sapply(dependancies, function(package){
-  library(package, character.only = TRUE)})
-
-dependancies_present <- sapply(dependancies, function(package){
-  package <- paste0("package:", package)
-  logic <- package %in% search()
-})
-
-if(FALSE %in% dependancies_present){
-  Unloaded_Packages <- data.frame(package=as.character(dependancies), 
-                                  loaded=dependancies_present)
-  stop("Load required packages. Check Unloaded_Packages for missing
-          dependancies.")
-  }else{
-  remove(dependancies, dependancies_present)
-  message("Required packages loaded.")
-}
-
-
-#Source function scripts
-function_scripts <- c("track_clones.R", 
-                      "determine_abundance.R", 
-                      "standardize_intsites.R", 
-                      "normalize_multihit_clusters.R",
-                      "remove_repeats.R",
-                      "db_to_granges.R",
-                      "condense_intsites.R")
-sapply(function_scripts, function(path){source(file = paste0("functions/", path))})
-
-
 #Using a keep_cols list, remove unwanted metadata from GRanges
 condense_metadata <- function(sites, keep_cols){
   tot_metadata <- names(mcols(sites))
