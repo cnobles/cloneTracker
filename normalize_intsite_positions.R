7232cad812b5033723235c71da268034055078f2
cnobles <chris.l.nobles@gmail.com>
2015-08-21 09:26:05 -0400

    Updated method to not rely on GRanges objects to determine the normalized position.

--- normalize_intsite_positions.R
+++ normalize_intsite_positions.R
@@ -1,60 +1,54 @@
 #Normalize the position of integration sites between repliates and samples
 normalize_intsite_positions <- function(sites, gap = 5L){
-  sites$order <- 1:length(sites)
-  mcols <- mcols(sites)
-  mcols(sites) <- NULL
-  sites$order <- 1:length(sites)
   sites$called.pos <- ifelse(strand(sites) == "+", start(sites), end(sites))
-  mcols$called.pos <- ifelse(strand(sites) == "+", start(sites), end(sites))
   sites$called.bp <- ifelse(strand(sites) == "+", end(sites), start(sites))
-  mcols$called.bp <- ifelse(strand(sites) == "+", end(sites), start(sites))
   sites <- flank(sites, -1, start = TRUE)
   
   graph.gap <- graphOverlaps(sites, gap = gap)
   clusters.gap <- clusters(graph.gap)
   clusters_to_normalize <- grep(TRUE, clusters.gap$csize > 1)
   
-  sites$clus <- clusters.gap$membership
-  mcols$clus <- clusters.gap$membership
+  sites$pos.clus <- clusters.gap$membership
+
+  norm.clus.dfl <- lapply(clusters_to_normalize, function(pos.clus){
+    obs <- grep(TRUE, sites$pos.clus == pos.clus)
+    dfr <- data.frame("strand" = strand(sites[obs]),
+                      "called.pos" = sites[obs]$called.pos,
+                      "pos.clus" = sites[obs]$pos.clus,
+                      stringsAsFactors = FALSE)
+    dfr
+  })
   
-  unnorm.sites <- sites[grep(TRUE, !sites$clus %in% clusters_to_normalize)]
-  norm.sites <- sites[grep(TRUE, sites$clus %in% clusters_to_normalize)]
-  
-  norm.sites <- unlist(GRangesList(lapply(unique(norm.sites$clus), function(clus){
-    gr <- norm.sites[norm.sites$clus == clus]
-    graph.0L <- graphOverlaps(gr, gap = 0L)
-    clusters.0L <- clusters(graph.0L)
-    gr$freq <- as.numeric(Rle(clusters.0L$csize, clusters.0L$csize))
-    gr$cliq <- clusters.0L$membership
-    max.cliq <- largest.cliques(graph.0L)
-    positions <- start(gr[unlist(max.cliq)])
-    gr$clus.pos <- round(median(positions))
-    gr
+  #Generate a key of the clusters and normalized positions
+  norm.key <- as.data.frame(bind_rows(lapply(norm.clus.dfl, function(pos.dfr){
+    pos.freq <- as.data.frame(table(pos.dfr$called.pos))
+    top.freq <- as.numeric(as.character(
+      pos.freq[pos.freq$Freq == max(pos.freq$Freq), "Var1"]
+    ))
+    norm.pos <- ifelse(unique(pos.dfr$strand) == "+", min(top.freq), max(top.freq))
+    key.row <- data.frame("norm.pos" = norm.pos,
+                          "pos.clus" = unique(pos.dfr$pos.clus),
+                          stringsAsFactors = FALSE)
+    key.row
   })))
+  unnorm.sites <- grep(TRUE, !sites$pos.clus %in% clusters_to_normalize)
+  unnorm.key <- data.frame("norm.pos" = sites[unnorm.sites]$called.pos,
+                           "pos.clus" = sites[unnorm.sites]$pos.clus,
+                           stringsAsFactors = FALSE)
+  clus.pos.key <- as.data.frame(bind_rows(norm.key, unnorm.key))
+  row.names(clus.pos.key) <- clus.pos.key$pos.clus
+  sites$norm.pos <- clus.pos.key[as.character(sites$pos.clus), "norm.pos"]
   
-  ranges <- IRanges(start = ifelse(strand(norm.sites) == "+", 
-                                   norm.sites$clus.pos, 
-                                   norm.sites$called.bp),
-                    end = ifelse(strand(norm.sites) == "+", 
-                                 norm.sites$called.bp, 
-                                 norm.sites$clus.pos))
-  normalized.sites <- GRanges(seqnames = seqnames(norm.sites),
-                       ranges = ranges,
-                       strand = strand(norm.sites),
-                       seqinfo = seqinfo(norm.sites))
-  
-  normalized.sites$order <- norm.sites$order
-  unnorm.sites$called.pos <- NULL
-  unnorm.sites$called.bp <- NULL
-  unnorm.sites$clus <- NULL
-  normalized.sites <- c(normalized.sites, unnorm.sites)
-  
-  mcols <- as.data.frame(mcols, row.names = mcols$order)[normalized.sites$order,]
-  mcols(normalized.sites) <- mcols
-  normalized.sites <- unlist(GRangesList(sapply(1:length(normalized.sites),
-                                                function(i){
-                                                  normalized.sites[normalized.sites$order == i]
-                                                })))
-  normalized.sites$order <- NULL
+  ranges <- IRanges(start = ifelse(strand(sites) == "+", 
+                                   sites$norm.pos, 
+                                   sites$called.bp),
+                    end = ifelse(strand(sites) == "+", 
+                                 sites$called.bp, 
+                                 sites$norm.pos))
+  normalized.sites <- GRanges(seqnames = seqnames(sites),
+                              ranges = ranges,
+                              strand = strand(sites),
+                              seqinfo = seqinfo(sites))
+  mcols(normalized.sites) <- mcols(sites)
   normalized.sites
 }
