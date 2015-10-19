1f982691126524106000abcdb15b5e002962cd88
cnobles <chris.l.nobles@gmail.com>
2015-10-19 17:29:03 -0400

    Modified test ranges.

--- ex_Data/test_GRanges.R
+++ ex_Data/test_GRanges.R
@@ -1,7 +1,7 @@
 #Test GRange
 test <- GRanges(seqnames = Rle(values = c("chr1", "chr2", "chr3"),
                                lengths = c(5, 5, 5)),
-                ranges = IRanges(start = c(2,7,10,7,8,15,15,17,16,14,6,7,6,5,2),
-                                 end = c(18,25,25,28,24,30,30,30,29,30,16,20,19,16,25)),
+                ranges = IRanges(start = c(2,7,10,7,8,15,10,5,16,17,6,8,6,5,4),
+                                 end = c(18,25,25,28,24,30,30,30,29,28,16,20,17,16,20)),
                 strand = Rle(values = c("+","-","+"),
                              lengths = c(5,5,5)))
