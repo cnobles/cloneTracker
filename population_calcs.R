986fc91babae73b7a0cfb75c5e665e772726ef65
cnobles <chris.l.nobles@gmail.com>
2016-01-26 17:09:48 -0500

    Removed NA's from sumations.

--- population_calcs.R
+++ population_calcs.R
@@ -2,7 +2,7 @@
 
 #Calculate the Shannon Diversity index given a set of frequencies / proportions
 calc_shannon <- function(x, base = exp(1)){
-  x <- x/sum(x)
+  x <- x/sum(x, na.rm = TRUE)
   shannon <- -x*log(x, base)
   shannon <- sum(shannon, na.rm = TRUE)
   shannon
@@ -11,21 +11,21 @@ calc_shannon <- function(x, base = exp(1)){
 #Calculate Gini Index for a sample given frequencies / proportions
 calc_gini <- function(x){
   stopifnot(require(reldist))
-  x <- x/sum(x)
+  x <- x/sum(x, na.rm = TRUE)
   gini <- gini(x)
   gini
 }
 
 #Calculate the Entropy of a sample set as defined by Adaptive Biotechnologies
 calc_entropy <- function(x){
-  x <- x/sum(x)
+  x <- x/sum(x, na.rm = TRUE)
   entropy <- -sum(x*log(x, base = 2))
   entropy
 }
 
 #Calculate the clonality as defined by Adaptive Biotechnologies for TCR and IgH sequence analysis
 calc_clonality <- function(x){
-  x <- x/sum(x)
+  x <- x/sum(x, na.rm = TRUE)
   clonality <- 1+sum(x*log(x, base = length(x)))
   clonality
 }
