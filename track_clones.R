95263899cfedcc7e8a36211bf7e5c457248e0b6d
cnobles <cnobles@upenn.edu>
2016-03-13 19:44:07 -0400

    Added graceful fail

--- track_clones.R
+++ track_clones.R
@@ -14,20 +14,28 @@ track_clones <- function(sites.list, gap=5L, track.origin=TRUE, standardize=TRUE
   }
   
   ovlp.grps <- findOverlaps(grl.sites, maxgap = gap, ignoreSelf = TRUE, ignoreRedundant = TRUE)
-  ovlp.sites <- unlist(GRangeslist(lapply(1:length(ovl.grps), function(i){
-    query <- grl.sites[[queryHits(ovl.grps[i])]]
-    subject <- grl.sites[[subjectHits(ovl.grps[i])]]
-    hits <- findOverlaps(query, subject, maxgap = gap)
-    c(query[queryHits(hits)], subject[subjectHits(hits)])
-  })))
-  
-  if(standardize){
-    std.sites <- standardize_intsites(ovlp.sites, std.gap = 1L, standardize_breakpoints = FALSE)
+  if(length(ovlp.grps) > 0){
+   ovlp.sites <- unlist(GRangesList(lapply(1:length(ovlp.grps), function(i){
+     query <- grl.sites[[queryHits(ovlp.grps[i])]]
+     subject <- grl.sites[[subjectHits(ovlp.grps[i])]]
+     hits <- findOverlaps(query, subject, maxgap = gap)
+     c(query[queryHits(hits)], subject[subjectHits(hits)])
+   })))
   }else{
-    std.sites <- ovlp.sites
+   message("No overlaping sites found between groups.")
+   std.list <- GRangesList()
   }
   
-  std.sites$posid <- generate_posID(std.sites)
-  std.list <- split(std.sites, std.sites$posid)
+  if(length(ovlp.grps) > 0){
+   if(standardize){
+     std.sites <- standardize_intsites(ovlp.sites, std.gap = 1L, standardize_breakpoints = FALSE)
+   }else{
+     std.sites <- ovlp.sites
+   }
+  
+   std.sites$posid <- generate_posID(std.sites)
+   std.list <- split(std.sites, std.sites$posid)
+  }
+  
   std.list
 }
