89f194d7d0f8a1b908960c5cc7fdd9bad1aab5b7
Chris Nobles <nobles@microb191.med.upenn.edu>
2016-07-11 09:50:33 -0400

    Updated standardizing method for multihit clusters.

--- normalize_multihit_clusters.R
+++ normalize_multihit_clusters.R
@@ -1,4 +1,4 @@
-normalize_multihit_clutsers <- function(multihits.gr, grouping = NULL){
+normalize_multihit_clutsers <- function(multihits.gr, gap = 5L, grouping = NULL){
   #Multihits must be standardized and have clusterID info
   if(is.null(grouping)){
     multihits.gp <- GRangesList(multihits.gr)
@@ -13,38 +13,33 @@ normalize_multihit_clutsers <- function(multihits.gr, grouping = NULL){
   }
   
   std.multi.gr <- lapply(multihits.gp, function(gr){
-    key <- data.frame("multihitID" = gr$multihitID,
-                      "clusterID" = as.character(gr$pos.clus)) 
-  
-    clusterID_to_position <- data.frame("clusterID" = unique(key$clusterID), 
-                                        "position" = seq(1:length(unique(key$clusterID))))
-  
-    key <- merge(key, clusterID_to_position, by.x="clusterID", by.y="clusterID")
-  
-    multi.art.gr <- GRanges(seqnames = Rle("art_chr", nrow(key)),
-                            ranges = IRanges(start = key$position, width = 1),
-                            strand = Rle("*", nrow(key)),
-                            multihitID = key$multihitID,
-                            clusterID = key$clusterID)
-  
-    multi.art.gl <- split(multi.art.gr, multi.art.gr$multihitID)
-    overlaps <- findOverlaps(multi.art.gl, minoverlap = 0L, select = "all", 
-                           ignoreSelf = FALSE, ignoreRedundant = FALSE)
-    edgelist <- matrix(c(queryHits(overlaps), subjectHits(overlaps)), ncol = 2)
-    clusters <- clusters(graph.edgelist(edgelist, directed = FALSE))
-  
-    clus.dfr <- data.frame("multihitID" = names(multi.art.gl),
-                         "stdmultihitID" = clusters$membership)
-  
-    key <- merge(key, clus.dfr, by.x = "multihitID", by.y = "multihitID")
-    multi.list <- split(gr, gr$multihitID)
-    multi.list <- lapply(multi.list, function(x){
-      multihitID <- unique(x$multihitID)
-      stdmultihitID <- key[key$multihitID == multihitID, 4]
-      x$stdmultihitID <- stdmultihitID
-      x
-    })
-    gr <- do.call(c, lapply(1:length(multi.list), function(i){multi.list[[i]]}))
+    key <- unique(data.frame(
+      "multihitID" = as.character(gr$multihitID),
+      "clusID" = gr$pos.clus,
+      stringsAsFactors = FALSE
+    )) 
+    
+    split.key <- split(key, key$clusID)
+    edgelist <- matrix(c(
+      Rle(
+        values = sapply(split.key, function(x) x$multihitID[1]),
+        lengths = sapply(split.key, nrow)
+      ),
+      unlist(sapply(split.key, function(x) x$multihitID))),
+      ncol = 2
+    )
+    graph <- graph.edgelist(edgelist, directed = FALSE)
+    std.multi.id <- clusters(graph)$membership
+    key$stdmultihitID <- std.multi.id[key$multihitID]
+    key$clusID <- NULL
+    key <- unique(key)
+    
+    std.key <- data.frame(
+      row.names = key$multihitID,
+      "stdmultihitID" = key$stdmultihitID
+    )
+    
+    gr$stdmultihitID <- std.key[as.character(gr$multihitID), "stdmultihitID"]    
     gr
   })
   
